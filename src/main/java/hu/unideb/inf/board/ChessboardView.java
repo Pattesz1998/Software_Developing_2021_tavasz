4dcbfc8b520f2dd365b26f18cea8d03f51632841
Pattesz1998 <kisp13@gmail.com>
2021-05-25 11:56:42 +0200

    Javadoc update for the ChessboardView.java (final)

--- src/main/java/hu/unideb/inf/board/ChessboardView.java
+++ src/main/java/hu/unideb/inf/board/ChessboardView.java
@@ -11,10 +11,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+/**
+ * In the ChessboardView class we got the visual representation of the gameboard.
+ */
+
 public class ChessboardView {
     private GridPane chessboardGrid;
     private FieldView[][] chessboard;
     private List<PieceMovedObserver> observers = new ArrayList<>();
+
+    /**
+     * Here we declare the colors of the different fields, and declare a size variable which type is integer. It stores the size of the gameboard.
+     * Color nonPlayable - Color of the non playable field
+     * Color playable - Color of the playable field
+     * Color prompt - Color of the prompt
+     * Color selected - Color of the selected field
+     * Color wolfColor - Color of the wolf player
+     * Color sheepColor - Color of the sheep player
+     */
 
     final int size;
     final static Color nonPlayable = Color.valueOf("#B8B8B8");
@@ -23,6 +37,12 @@ public class ChessboardView {
     final static Color selected = Color.valueOf("#908F09");
     public final static Color wolfColor = Color.valueOf("#9C2D2A");
     public final static Color sheepColor = Color.valueOf("#00B2EE");
+
+    /**
+     * Here we need the chessboardModel class, which makes our gameboard, but for the visual representation we need the Model aswell so we have to integrate the Model class..
+     * FieldView[size][size], here the size will be 8x8, because this game is tradicionally plays on the chess gameboard which states from 8x8 = total of 64 fields.
+     * with for cycle we have to go through the rows and columns of the chessboard and create the fields visually..
+     */
 
     public ChessboardView(ChessboardModel chessboardModel) {
         this.size = ChessboardModel.size;
